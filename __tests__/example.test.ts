e9bc7c3814247533e06d83b7018240856b37c31d
dave.noh <woojenoh@gmail.com>
2022-05-27 16:32:39 +0900

    테스트 작성

---
+++ __tests__/example.test.ts
@@ -0,0 +1,36 @@
+import puppeteer, { Browser, Page } from "puppeteer";
+import { PendingXHR } from "pending-xhr-puppeteer";
+import { toMatchImageSnapshot } from "jest-image-snapshot";
+
+expect.extend({ toMatchImageSnapshot });
+
+describe("page", () => {
+  let browser: Browser;
+  let page: Page;
+  let pendingXHR: PendingXHR;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      defaultViewport: null,
+      args: ["--window-size=1920,1920"],
+    });
+  });
+  beforeEach(async () => {
+    page = await browser.newPage();
+    pendingXHR = new PendingXHR(page);
+  });
+  afterEach(async () => {
+    await page.close();
+  });
+  afterAll(async () => {
+    await browser.close();
+  });
+
+  it("match", async () => {
+    await page.goto(`http://localhost:3000`);
+    await pendingXHR.waitForAllXhrFinished();
+    await page.waitForTimeout(1000);
+    const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot();
+  });
+});
