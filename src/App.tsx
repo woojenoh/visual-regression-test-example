2406340becb9dbc60511335491b791a798c5fbd3
노우제 <woojenoh@gmail.com>
2022-05-29 01:09:27 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Visual Regression Test
+          Learn!
         </a>
       </header>
     </div>
