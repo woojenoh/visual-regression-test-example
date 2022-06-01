bd813e8fc8775352b30d1c2f1e29e207b3beb504
노우제 <woojenoh@gmail.com>
2022-06-01 18:21:42 +0900

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,14 @@ function App() {
   return (
     <>
       <div>
-        <Link to={PATH.HOME}>Home</Link>
+        <Link to={PATH.HOME}>Home - Younha</Link>
         {" | "}
-        <Link to={PATH.ABOUT}>About</Link>
+        <Link to={PATH.ABOUT}>About Time</Link>
       </div>
       <Routes>
         <Route
           path={PATH.HOME}
-          element={<span>Learn Visual Regression Test</span>}
+          element={<span>Learn React!</span>}
         />
         <Route
           path={PATH.ABOUT}
@@ -27,7 +27,7 @@ function App() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              About Me
+              About You
             </a>
           }
         />
