f0206cc5d8182b7db713c416c4d6d59588a925d3
노우제 <woojenoh@gmail.com>
2022-06-01 17:38:19 +0900

    수정

--- .github/scripts/get-diff-urls.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-DIFF_URLS=""
-
-for d in $(aws s3 ls s3://vrt-bucket/__diff_output__/ | awk '{print $4}' | grep diff); do
-  DIFF_URLS+="![DIFF_URL]("https://vrt-bucket.s3.ap-northeast-2.amazonaws.com/__diff_output__/$d")"
-done
-
-echo "::set-output name=DIFF_URLS::$(DIFF_URLS)"
