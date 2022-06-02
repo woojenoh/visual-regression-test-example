92ef6e5ac48e610e853837cb04b61721fbd5f1dc
dave.noh <woojenoh@gmail.com>
2022-06-02 14:07:46 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              About Me
+              About @@@
             </a>
           }
         />
